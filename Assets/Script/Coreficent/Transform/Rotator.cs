01aec9e266e71372e704b8a653b9d3d2b10de6f1
Stephen Xie <coreficent@gmail.com>
2020-11-28 03:30:26 -0500

    Implement simple rotation.

--- Assets/Script/Coreficent/Transform/Rotator.cs
+++ Assets/Script/Coreficent/Transform/Rotator.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+
+    private float speed = 20f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal") * speed * Mathf.Deg2Rad;
+        float y = Input.GetAxis("Vertical") * speed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -x);
+        transform.Rotate(Vector3.right, y);
     }
 }
