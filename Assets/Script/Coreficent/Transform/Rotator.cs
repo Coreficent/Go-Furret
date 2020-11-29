7f4ac2ee2468111147b4307f6fab61097b1d0fb9
Stephen Xie <coreficent@gmail.com>
2020-11-29 10:24:47 -0500

    Implement global rotation.

--- Assets/Script/Coreficent/Transform/Rotator.cs
+++ Assets/Script/Coreficent/Transform/Rotator.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Rotator : MonoBehaviour
+﻿namespace Coreficent.Transform
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-    private float speed = 20f;
-    // Start is called before the first frame update
-    void Start()
+    public class Rotator : MonoBehaviour
     {
+        private readonly float _rotationSpeed = 45.0f;
+        private readonly string _verticalControl = "Vertical";
+        private readonly string _horizontalControl = "Horizontal";
 
-    }
+        void Update()
+        {
+            ControlRotation();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float x = Input.GetAxis("Horizontal") * speed * Mathf.Deg2Rad;
-        float y = Input.GetAxis("Vertical") * speed * Mathf.Deg2Rad;
-
-        transform.Rotate(Vector3.up, -x);
-        transform.Rotate(Vector3.right, y);
+        private void ControlRotation()
+        {
+            transform.rotation *= Quaternion.AngleAxis(_rotationSpeed * Input.GetAxis(_horizontalControl) * Time.deltaTime, transform.InverseTransformDirection(-Vector3.up));
+            transform.rotation *= Quaternion.AngleAxis(_rotationSpeed * Input.GetAxis(_verticalControl) * Time.deltaTime, transform.InverseTransformDirection(-Vector3.left));
+        }
     }
 }
