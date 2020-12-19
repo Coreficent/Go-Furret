791da7e525d3319db951d12a40cccd7f2867eaa7
Stephen Xie <coreficent@gmail.com>
2020-12-19 06:38:45 -0500

    Implement primitive control.

--- Assets/Script/Coreficent/Main/Main.cs
+++ Assets/Script/Coreficent/Main/Main.cs
@@ -4,18 +4,29 @@
     using System.Collections.Generic;
     using UnityEngine;
     using Coreficent.Utility;
+    using Coreficent.Control;
 
     public class Main : MonoBehaviour
     {
         public GameObject Furret;
+
+        private FurretControl _furretControl;
         private void Start()
         {
             SanityCheck.Check(this, Furret);
-            Furret.GetComponent<Animator>().SetBool("Started", true);
+            _furretControl = new FurretControl(Furret.GetComponent<Animator>());
         }
         private void Update()
         {
-
+            /*
+                check for user input
+                run AI
+                move enemies
+                resolve collisions
+                draw graphics
+                play sounds
+             */
+            _furretControl.Run();
         }
     }
 }
