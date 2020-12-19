6c69bfd5bd2c2dbe35a2927aafc753a887c80746
Stephen Xie <coreficent@gmail.com>
2020-12-19 05:47:10 -0500

    Add main.

---
+++ Assets/Script/Coreficent/Main/Main.cs
@@ -0,0 +1,20 @@
+﻿namespace Coreficent.Main
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Coreficent.Utility;
+
+    public class Main : MonoBehaviour
+    {
+        public GameObject Furret;
+        private void Start()
+        {
+            SanityCheck.Check(this, Furret);
+        }
+        private void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Script/Coreficent/Utility/SanityCheck.cs
@@ -0,0 +1,28 @@
+﻿namespace Coreficent.Utility
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class SanityCheck
+    {
+        public static bool DebugMode = false;
+
+        public static void Check(object owner, params object[] variables)
+        {
+            bool sanityCheckPassed = true;
+            foreach (object i in variables)
+            {
+                if (i == null)
+                {
+                    sanityCheckPassed = false;
+                    Debug.Log(owner + "::" + "has an unexpected null variable in" + "::" + SceneManager.GetActiveScene().name);
+                }
+            }
+
+            if (DebugMode && sanityCheckPassed)
+            {
+                Debug.Log(owner + "::" + "sanity check passed.");
+            }
+        }
+    }
+}
