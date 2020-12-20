34948f1794e81484890acfe48da5d6a6bdee3368
Stephen Xie <coreficent@gmail.com>
2020-12-20 02:26:49 -0500

    Implement primitive character rotation.

--- Assets/Script/Coreficent/Control/PlayerController.cs
+++ Assets/Script/Coreficent/Control/PlayerController.cs
@@ -6,7 +6,12 @@
 
     public class PlayerController : MonoBehaviour
     {
+        private readonly string _verticalControl = "Vertical";
+        private readonly string _horizontalControl = "Horizontal";
+        private float _walkSpeed = 50.0f;
+        private float _rotationSpeed = 50.0f;
         private Animator _animator;
+
 
         private void Start()
         {
@@ -16,7 +21,13 @@
         public void Update()
         {
             _animator.SetBool("Started", Input.GetKey(KeyCode.S));
-            transform.rotation = Quaternion.LookRotation(transform.position * -1.0f, Vector3.down);
+
+            Vector3 inputDirection = new Vector3(Input.GetAxis(_horizontalControl), Input.GetAxis(_verticalControl));
+
+            if(inputDirection.magnitude > 0.5f)
+            {
+                transform.rotation = Quaternion.LookRotation(transform.position * -1.0f, inputDirection.normalized);
+            }
         }
     }
 }
