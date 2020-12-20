8fc3856d3a3a083df9768fc95f2bfc94a0cbfbf9
Stephen Xie <coreficent@gmail.com>
2020-12-20 02:39:20 -0500

    Implement smooth player rotation.

--- Assets/Script/Coreficent/Control/PlayerController.cs
+++ Assets/Script/Coreficent/Control/PlayerController.cs
@@ -9,7 +9,7 @@
         private readonly string _verticalControl = "Vertical";
         private readonly string _horizontalControl = "Horizontal";
         private float _walkSpeed = 50.0f;
-        private float _rotationSpeed = 50.0f;
+        private float _rotationSpeed = 10.0f;
         private Animator _animator;
 
 
@@ -20,13 +20,13 @@
 
         public void Update()
         {
-            _animator.SetBool("Started", Input.GetKey(KeyCode.S));
+            _animator.SetBool("Started", Input.GetKey(KeyCode.Alpha1));
 
-            Vector3 inputDirection = new Vector3(Input.GetAxis(_horizontalControl), Input.GetAxis(_verticalControl));
+            Vector3 inputDirection = new Vector2(Input.GetAxis(_horizontalControl), Input.GetAxis(_verticalControl)).normalized;
 
-            if(inputDirection.magnitude > 0.5f)
+            if (inputDirection.magnitude > 0.5f)
             {
-                transform.rotation = Quaternion.LookRotation(transform.position * -1.0f, inputDirection.normalized);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(transform.position * -1.0f, inputDirection), _rotationSpeed * Time.deltaTime);
             }
         }
     }
