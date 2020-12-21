35caf82a026ef9a8051b84698e32fad669fe0aa9
Stephen Xie <coreficent@gmail.com>
2020-12-21 04:08:59 -0500

    Implement gravity.

---
+++ Assets/Script/Coreficent/Physics/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace Coreficent.Physics
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Entity : MonoBehaviour
+    {
+        public GameObject Planet;
+        private void Awake()
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+
+        private void FixedUpdate()
+        {
+            Planet.GetComponent<Planet>().CalculatePhysics(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Coreficent/Physics/Planet.cs
@@ -0,0 +1,23 @@
+﻿namespace Coreficent.Physics
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    public class Planet : MonoBehaviour
+    {
+        public float Gravity = -50.0f;
+
+        private void Awake()
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+        public void CalculatePhysics(GameObject entity)
+        {
+            Vector3 to = (entity.transform.position - transform.position).normalized;
+            entity.transform.rotation = Quaternion.FromToRotation(entity.transform.up.normalized, to) * entity.transform.rotation;
+            entity.GetComponent<Rigidbody>().AddForce(to * Gravity);
+        }
+    }
+}
+
