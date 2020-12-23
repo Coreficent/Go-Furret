3685d1968bf2e90b5355e71517b07ee201d5ead9
Stephen Xie <coreficent@gmail.com>
2020-12-23 09:52:31 -0500

    Clean up.

--- Assets/Script/Coreficent/Controller/PlayerController.cs
+++ Assets/Script/Coreficent/Controller/PlayerController.cs
@@ -1,27 +1,18 @@
 ﻿namespace Coreficent.Physics
 {
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
 
-    public class Entity : MonoBehaviour
+    public class PlayerController : MonoBehaviour
     {
         public GameObject Planet;
 
         private float _turnAmount = 250.0f;
         private float _walkSpeed = 250.0f;
-        private Vector3 _inputDirection = new Vector3();
-
 
         private void Awake()
         {
             GetComponent<Rigidbody>().useGravity = false;
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-        }
-
-        private void Update()
-        {
-
         }
 
         private void FixedUpdate()
