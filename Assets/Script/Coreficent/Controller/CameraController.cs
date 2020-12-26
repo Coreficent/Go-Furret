bd1f6dcf91d4b3d67476c1cdd7f55bc686a9b405
Stephen Xie <coreficent@gmail.com>
2020-12-26 09:17:53 -0500

    Clean up camera.

--- Assets/Script/Coreficent/Controller/CameraController.cs
+++ Assets/Script/Coreficent/Controller/CameraController.cs
@@ -6,8 +6,8 @@
     public class CameraController : MonoBehaviour
     {
         public GameObject Player;
-        public float OffsetX = 1.0f;
-        public float OffsetY = -1.0f;
+        public float HorizontalOffset = -1.0f;
+        public float VerticalOffset = 1.0f;
 
         private void Start()
         {
@@ -19,8 +19,8 @@
             Transform playerTransform = Player.transform;
             Vector3 playerPosition = playerTransform.position;
 
-            Vector3 verticalPosition = playerPosition + playerTransform.TransformVector(new Vector3(0.0f, 2.0f, 0.0f));
-            Vector3 horizontalPosition = playerTransform.TransformVector(new Vector3(0.0f, 0.0f, -2.0f));
+            Vector3 verticalPosition = playerPosition + playerTransform.TransformVector(new Vector3(0.0f, VerticalOffset, 0.0f));
+            Vector3 horizontalPosition = playerTransform.TransformVector(new Vector3(0.0f, 0.0f, HorizontalOffset));
 
             DebugRender.Draw(playerTransform.position, verticalPosition, Color.black);
             DebugRender.Draw(verticalPosition, verticalPosition + horizontalPosition, Color.black);
