c9632279e187e8a58584145f6642af2720047def
Stephen Xie <coreficent@gmail.com>
2020-12-26 12:03:08 -0500

    Optimize player control.

--- Assets/Script/Coreficent/Controller/PlayerController.cs
+++ Assets/Script/Coreficent/Controller/PlayerController.cs
@@ -1,28 +1,42 @@
 ﻿namespace Coreficent.Controller
 {
     using Coreficent.Physics;
+    using Coreficent.Utility;
     using UnityEngine;
 
     public class PlayerController : MonoBehaviour
     {
         public GameObject Planet;
+        public float Speed = 0.0f;
+
+        private Rigidbody _rigidbody = null;
+        private Planet _planet = null;
+
+        private Vector3 _velocity = new Vector3();
 
         private float _turnAmount = 250.0f;
         private float _walkSpeed = 2.5f;
 
         private void Awake()
         {
-            GetComponent<Rigidbody>().useGravity = false;
-            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+            _rigidbody = GetComponent<Rigidbody>();
+            _planet = Planet.GetComponent<Planet>();
+
+            SanityCheck.Check(this, Planet, _rigidbody, _planet);
+
+            _rigidbody.useGravity = false;
+            _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
         }
 
         private void FixedUpdate()
         {
-            Planet.GetComponent<Planet>().CalculatePhysics(gameObject);
+            _planet.CalculatePhysics(gameObject);
 
             transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * _turnAmount * Time.fixedDeltaTime);
 
-            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"))) * _walkSpeed * Time.fixedDeltaTime);
+            Speed = Input.GetAxis("Vertical") * _walkSpeed;
+            _velocity.z = Speed * Time.fixedDeltaTime;
+            _rigidbody.MovePosition(_rigidbody.position + transform.TransformDirection(_velocity));
         }
     }
 }
