da5d56e5a0622cc94f4cdcfd13a146b2fdf3cf4c
Stephen Xie <coreficent@gmail.com>
2021-01-03 06:23:33 -0500

    Update animation control.

--- Assets/Script/Coreficent/Controller/AnimationController.cs
+++ Assets/Script/Coreficent/Controller/AnimationController.cs
@@ -8,12 +8,12 @@
         [SerializeField] private PlayerController _playerController;
         [SerializeField] private Animator _playerAnimator;
 
-        private void Start()
+        protected void Start()
         {
             SanityCheck.Check(this, _playerController, _playerAnimator);
         }
 
-        void Update()
+        protected void Update()
         {
             _playerAnimator.SetFloat("Speed", _playerController.Speed);
             _playerAnimator.SetBool("Jumping", !_playerController.Landed);
