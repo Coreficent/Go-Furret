f3fc1e120bb2168418df5b5ae4276056ece2e49b
Stephen Xie <coreficent@gmail.com>
2021-01-03 09:56:45 -0500

    Implement fruit.

---
+++ Assets/Script/Coreficent/Food/Fruit.cs
@@ -0,0 +1,30 @@
+﻿namespace Food.Fruit
+{
+    using Coreficent.Utility;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Fruit : MonoBehaviour
+    {
+        private readonly List<SkinnedMeshRenderer> _skinnedMeshRenderers = new List<SkinnedMeshRenderer>();
+
+        protected void Start()
+        {
+            foreach (Transform i in transform.Find("Display").transform)
+            {
+                SkinnedMeshRenderer skinnedMeshRenderer = i.GetComponent<SkinnedMeshRenderer>();
+                if (skinnedMeshRenderer != null)
+                {
+                    _skinnedMeshRenderers.Add(skinnedMeshRenderer);
+                }
+            }
+
+            DebugLogger.Start(this);
+        }
+
+        protected void Update()
+        {
+
+        }
+    }
+}
