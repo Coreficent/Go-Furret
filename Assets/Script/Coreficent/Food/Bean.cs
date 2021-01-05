a403ff98361b4d9f25ac51ef9ae8e0a49063bf05
Stephen Xie <coreficent@gmail.com>
2021-01-05 07:46:54 -0500

    Add bean and recipe groundwork.

---
+++ Assets/Script/Coreficent/Food/Bean.cs
@@ -0,0 +1,22 @@
+﻿namespace Coreficent.Food
+{
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Bean : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        protected void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        protected void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Script/Coreficent/Food/Recipe.cs
@@ -0,0 +1,24 @@
+﻿namespace Coreficent.Food
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Recipe : MonoBehaviour
+    {
+
+
+
+        // Start is called before the first frame update
+        protected void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        protected void Update()
+        {
+
+        }
+    }
+}
