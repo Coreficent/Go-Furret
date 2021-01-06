4330a514fdc2a6a21d6644b378d046fd3896f888
Stephen Xie <coreficent@gmail.com>
2021-01-06 16:40:13 -0500

    Improve bean property in recipe.

--- Assets/Script/Coreficent/Food/Recipe.cs
+++ Assets/Script/Coreficent/Food/Recipe.cs
@@ -6,9 +6,7 @@
 
     public class Recipe : MonoBehaviour
     {
-        [SerializeField] private Bean _regularBean;
-
-        private Bean _bean;
+        [SerializeField] private Bean _bean;
 
         public Bean CurrentBean
         {
@@ -17,9 +15,7 @@
 
         protected void Start()
         {
-            _bean = _regularBean;
-
-            SanityCheck.Check(this, _regularBean, _bean);
+            SanityCheck.Check(this, _bean);
         }
 
         public bool CanProduce(List<Fruit> fruits)
@@ -35,18 +31,19 @@
                     DebugLogger.Warn("tried to produce without a fruit");
                     break;
                 case 1:
-                    _bean = _regularBean;
                     _bean.Pooled = false;
                     _bean.transform.localScale *= 0.1f;
                     _bean.transform.position = transform.position + transform.TransformDirection(new Vector3(0.0f, 1.0f, 0.0f));
                     _bean.Color = fruits[0].Color;
                     break;
 
-
                 case 2:
+
                     break;
+
                 case 3:
                     break;
+
                 default:
                     break;
             }
