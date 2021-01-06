d2d607023674065f602ca998a1aea3ebd3f2f2a9
Stephen Xie <coreficent@gmail.com>
2021-01-06 17:04:31 -0500

    Implement two item recipe.

--- Assets/Script/Coreficent/Food/Recipe.cs
+++ Assets/Script/Coreficent/Food/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿namespace Coreficent.Food
 {
+    using Coreficent.Setting;
     using Coreficent.Utility;
     using System.Collections.Generic;
     using UnityEngine;
@@ -31,14 +32,55 @@
                     DebugLogger.Warn("tried to produce without a fruit");
                     break;
                 case 1:
-                    _bean.Pooled = false;
-                    _bean.transform.localScale *= 0.1f;
-                    _bean.transform.position = transform.position + transform.TransformDirection(new Vector3(0.0f, 1.0f, 0.0f));
+                    SummonBean();
                     _bean.Color = fruits[0].Color;
+                    _bean.Pattern = Bean.BeanPattern.Gray;
                     break;
 
                 case 2:
+                    SummonBean();
+                    _bean.Color = Color.white;
 
+                    _bean.Pattern = Bean.BeanPattern.Gray;
+                    if (fruits[0].Species == Fruit.FruitSpecies.Razz && fruits[1].Species == Fruit.FruitSpecies.Pinap)
+                    {
+                        _bean.Pattern = Bean.BeanPattern.Grass;
+                    }
+                    else if (fruits[0].Species == Fruit.FruitSpecies.Razz && fruits[1].Species == Fruit.FruitSpecies.Nanab)
+                    {
+                        _bean.Pattern = Bean.BeanPattern.Fire;
+                    }
+                    else if (fruits[0].Species == Fruit.FruitSpecies.Pinap && fruits[1].Species == Fruit.FruitSpecies.Razz)
+                    {
+                        _bean.Pattern = Bean.BeanPattern.Water;
+                    }
+                    else if (fruits[0].Species == Fruit.FruitSpecies.Pinap && fruits[1].Species == Fruit.FruitSpecies.Nanab)
+                    {
+                        _bean.Pattern = Bean.BeanPattern.Electric;
+                    }
+                    else if (fruits[0].Species == Fruit.FruitSpecies.Nanab && fruits[1].Species == Fruit.FruitSpecies.Razz)
+                    {
+                        _bean.Pattern = Bean.BeanPattern.Ice;
+                    }
+                    else if (fruits[0].Species == Fruit.FruitSpecies.Nanab && fruits[1].Species == Fruit.FruitSpecies.Pinap)
+                    {
+                        if (Configuration.Singleton.Version == Configuration.ApplicationVersion.Nova)
+                        {
+                            _bean.Pattern = Bean.BeanPattern.Fighting;
+                        }
+                        else if (Configuration.Singleton.Version == Configuration.ApplicationVersion.Umbra)
+                        {
+                            _bean.Pattern = Bean.BeanPattern.Ghost;
+                        }
+                        else
+                        {
+                            DebugLogger.Warn("unexpected application version from configuration");
+                        }
+                    }
+                    else
+                    {
+                        DebugLogger.Warn("unexpected pattern combination for two item recipe");
+                    }
                     break;
 
                 case 3:
@@ -47,6 +89,13 @@
                 default:
                     break;
             }
+        }
+
+        private void SummonBean()
+        {
+            _bean.Pooled = false;
+            _bean.transform.localScale *= 0.01f;
+            _bean.transform.position = transform.position + transform.TransformDirection(new Vector3(0.0f, 1.0f, 0.0f));
         }
     }
 }
