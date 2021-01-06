cdad75d702530865fd37a6ae0c1fd73b73e78bee
Stephen Xie <coreficent@gmail.com>
2021-01-06 09:46:23 -0500

    Implement primitive recipe.

--- Assets/Script/Coreficent/Food/Recipe.cs
+++ Assets/Script/Coreficent/Food/Recipe.cs
@@ -1,21 +1,17 @@
 ﻿namespace Coreficent.Food
 {
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
 
     public class Recipe : MonoBehaviour
     {
-        // Start is called before the first frame update
         protected void Start()
         {
 
         }
 
-        // Update is called once per frame
-        protected void Update()
+        public Bean Produce(Fruit fruit)
         {
-
+            return null;
         }
     }
 }
