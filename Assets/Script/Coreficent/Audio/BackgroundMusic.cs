7b1cd59149e9a1374f9b2eda7efc884a716277fd
Stephen Xie <coreficent@gmail.com>
2021-01-09 18:07:24 -0500

    Clean up music.

--- Assets/Script/Coreficent/Audio/BackgroundMusic.cs
+++ Assets/Script/Coreficent/Audio/BackgroundMusic.cs
@@ -1,35 +1,14 @@
 ﻿namespace Coreficent.Audio
 {
-    using Coreficent.Utility;
     using UnityEngine;
 
     public class BackgroundMusic : MonoBehaviour
     {
-        private static BackgroundMusic Singleton = null;
-
         public AudioSource Music;
 
         protected void Start()
         {
-            if (!Singleton)
-            {
-                SanityCheck.Check(this, Music);
-
-                Singleton = this;
-                DontDestroyOnLoad(gameObject);
-                Music.Play();
-
-                return;
-            }
-            else if (Singleton == this)
-            {
-                return;
-            }
-            else
-            {
-                Destroy(gameObject);
-                return;
-            }
+            Music.Play();
         }
     }
 }
